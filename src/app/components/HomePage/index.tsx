bad6ed79015f824feb5677b6518309da9aa9e377
Saurabhsanjay <saurabhubale371@gmail.com>
2025-02-11 15:38:25 +0530

    folder updated

---
+++ src/app/components/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WhyChooseLynk from './WhyChooseLynk'
+
+const HomePage = () => {
+    return (
+        <div>
+            <WhyChooseLynk />
+        </div>
+    )
+}
+
+export default HomePage;
