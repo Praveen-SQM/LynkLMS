1ffe68da1a4b1b8b5eb71e47f72ddf540b91cb02
Saurabhsanjay <saurabhubale371@gmail.com>
2025-02-11 15:36:34 +0530

    components added

---
+++ src/app/components/HomePage/WhyChooseLynk.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WhyChooseLynk = () => {
+  return (
+      <div className='w-[1920px] h-[1877px] bg-[#FAFAFA]'>
+      
+    </div>
+  )
+}
+
+export default WhyChooseLynk
