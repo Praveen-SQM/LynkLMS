c66cf3a3ac1f44875f2fcd3c90448fe77f018018
Kamal Sharma <kamal.s@squadramedia.com>
2025-02-12 12:44:31 +0530

    resolved structure

--- src/app/components/HomePage/index.tsx
+++ src/app/components/HomePage/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import WhyChooseLynk from './WhyChooseLynk'
-import TabSwitch from './tabSwitch'
+import React from "react";
+import WhyChooseLynk from "./WhyChooseLynk";
+import TabSwitch from "./tabSwitch";
+import { NavbarTop } from "./NavbarMenu/Navbar";
+import LandingHero from "./HeroSection/LandingHero";
+import LMSFeatures from "./LMSFeatures/LMSFeatures";
 
 const HomePage = () => {
-    return (
-        <div>
-            <WhyChooseLynk />
-            <TabSwitch/>
+  return (
+    <div>
+      <NavbarTop />
+      <div className="w-full bg-gray-50 px-4">
+        <div className="flex items-center h-screen max-h-[1053px] w-full pt-16 mx-auto max-w-6xl">
+          <LandingHero />
         </div>
-    )
-}
+      </div>
+      <LMSFeatures />
+      <WhyChooseLynk />
+      <TabSwitch />
+    </div>
+  );
+};
 
 export default HomePage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 "use client";
-
-import LandingHero from "@/components/ui/HeroSection/LandingHero";
-import { NavbarTop } from "@/components/ui/NavbarMenu/Navbar";
-import LMSFeatures from "@/components/ui/LMSFeatures/LMSFeatures";
+import HomePage from "./components/HomePage";
 export default function Home() {
   return (
     <div className="w-full">
-      <NavbarTop />
+      {/* <NavbarTop />
       <div className="w-full bg-gray-50 px-4">
         <div className="flex items-center h-screen max-h-[1053px] w-full pt-16 mx-auto max-w-6xl">
           <LandingHero />
         </div>
       </div>
-      <LMSFeatures />
+      <LMSFeatures /> */}
+      <HomePage />
     </div>
   );
 }
