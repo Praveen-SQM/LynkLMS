aa507e1ca19b468fdbf57fa2562f32e2030c0015
Kuldeep Singh Jadon <kuldeep@squadramedia.com>
2025-02-18 10:27:33 +0530

    made development process section

--- src/app/components/E-learning/Index.tsx
+++ src/app/components/E-learning/Index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import TrainingContent from "./ELearning";
+import DevelopmentProcessSection from "./DevelopmentProcessSection";
+import BenefitsSection from "./BenefitsSection";
 
 const ELearning = () => {
   return (
     <div>
       {" "}
       <TrainingContent />
+      <DevelopmentProcessSection/>
+      <BenefitsSection/>
     </div>
   );
 };
