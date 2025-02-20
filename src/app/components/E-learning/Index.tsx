1c35c5f0b3007acf97a78dbdcf003ce4a7da2419
Kuldeep Singh Jadon <kuldeep@squadramedia.com>
2025-02-20 11:16:05 +0530

    added experts section

--- src/app/components/E-learning/Index.tsx
+++ src/app/components/E-learning/Index.tsx
@@ -1,8 +1,12 @@
+'use client'
 import React from "react";
 import TrainingContent from "./ELearning";
 import DevelopmentProcessSection from "./DevelopmentProcessSection";
 import BenefitsSection from "./BenefitsSection";
 import ELearningLanding from "./ELearningLanding";
+import LMSBenefits from "@/app/components/HomePage/LMSBenifits/LMSBenifits"
+import FAQSection from "../HomePage/FAQSeaction";
+import ExpertsSection from "./ExpertsSection";
 
 const ELearning = () => {
   return (
@@ -11,7 +15,10 @@ const ELearning = () => {
       <ELearningLanding />
       <TrainingContent />
       <DevelopmentProcessSection/>
+      <ExpertsSection/>
+      <LMSBenefits/>
       <BenefitsSection/>
+      <FAQSection/>
     </div>
   );
 };
