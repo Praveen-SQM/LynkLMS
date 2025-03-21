6d2ad3dd5685347607cbab5ac177a897767261bc
Saurabhsanjay <saurabhubale371@gmail.com>
2025-03-21 14:35:48 +0530

    feat: add Contentsquare tracking script to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,21 @@ export default function RootLayout({
           />
         </noscript>
 
+        {/* Contentsquare */}
+        <Script id="contentsquare" strategy="afterInteractive">
+          {`
+    (function (c, s, q, u, a, r, e) {
+    c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
+    c._hjSettings = { hjid: a };
+    r = s.getElementsByTagName('head')[0];
+    e = s.createElement('script');
+    e.async = true;
+    e.src = q + c._hjSettings.hjid + u;
+    r.appendChild(e);
+})(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 5345975);
+  `}
+        </Script>
+
         {/* Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
