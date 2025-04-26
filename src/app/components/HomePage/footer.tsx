10b6a715594945ad70540a70d418b3a9a22049ca
Kuldeep Singh Jadon <kuldeep@squadramedia.com>
2025-04-26 09:58:37 +0530

    fix: update leadSource to 'Lynk Landing Page' in footer and contact modal submissions

--- src/app/components/HomePage/footer.tsx
+++ src/app/components/HomePage/footer.tsx
@@ -229,7 +229,7 @@ const Footer = () => {
           phoneNumber: "Not Provided",
           message:
             "Requested a callback on email provided for more information from lynk lms",
-          leadSource: "Lynk LMS Website",
+          leadSource: "Lynk Landing Page",
         });
         setIsSubmitting(false);
         setSubmitted(true);
--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -257,7 +257,7 @@ const ContactForm: React.FC = () => {
                 toast.success(result.message, { duration: 3000 });
                 await axios.post('/api/zoho', {
                     ...formData,
-                    "leadSource":"Lynk LMS Website"
+                    "leadSource":"Lynk Landing Page"
                 });
                 setFormData({
                     firstName: '',
