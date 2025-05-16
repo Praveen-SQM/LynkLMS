d78387bde293e437bb3ac568fad3064bf26825d8
vivekanand <vivekanand@squadramedia.com>
2025-05-16 18:15:14 +0530

    fix: update Google Tag Manager integration for improved tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,15 +62,19 @@ export default function RootLayout({
         </Script>
 
         {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17084294991"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], 
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); 
-            })(window,document,'script','dataLayer','GTM-NQ7B96J3');
-          `}
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'AW-17084294991');
+  `}
         </Script>
+
         {/* End Google Tag Manager */}
       </head>
       <body className={`${manrope.variable} antialiased`}>
